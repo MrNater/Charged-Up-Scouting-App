8208e476749655a97c6920d8a68b68d8bf9895dd
MrNater <ntdarty@gmail.com>
2024-02-17 14:30:25 -0600

    Test- Made button do something

--- app/src/main/java/com/example/chargedupscoutingapp/MainActivity.java
+++ app/src/main/java/com/example/chargedupscoutingapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.chargedupscoutingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.firstpage);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the Supabutton");
+            }
+        });
     }
 }
